9b4585b13480104b8763012170ce526ad07479f7
Ganesh <2110080050@klh.edu.in>
2024-09-18 17:25:55 +0530

    updated logo

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -14,7 +14,7 @@ useEffect(() => {
 
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-md'>
-        <Image src="/logo.svg" alt="logo" width={150} height={100}  />
+        <Image src="/logo2.svg" alt="logo" width={150} height={100}  />
         <ul className='hidden md:flex gap-6'>
             <li className={`hover: text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
             <li className={`hover: text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
--- app/page.js
+++ app/page.js
@@ -8,7 +8,10 @@ export default function Home() {
   return (
     <div>
       <h1>AI Mock Interview</h1>
-      <Image src="/vercel.svg" alt="logo" width={200} height={500} />
+      <Image src="/logo2.svg" alt="logo" width={200} height={200} />
+      {/* logo maker */}
+      {/* https://studio.creativefabrica.com/vectorizer/2mEyZx1qCYco90aBhT0M6a7EkV3 */}
+      {/* https://www.design.com/maker/logo/6065b164-80a9-44f7-ab07-f902d53b5843/draft/b2dd4259-d98e-47e7-9d06-de53d06c89db */}
       
       <Link href={`/dashboard`}>
         <Button>Click me</Button>
